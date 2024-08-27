9abb4ad80a19d098f1ef0f2d2f723cae9d56ebdc
Leonardo250 <leomoulin2002@gmail.com>
2024-08-27 01:16:43 -0300

    Teste

--- Primeiro-Projeto/src/App.java
+++ Primeiro-Projeto/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int a = 3;
+        int b = 4;
+        int c = a+b;
+        System.out.println("Hello, World! " + c);
     }
 }
